f6a5e76dc5ebc6e8b326c3ed2df7a99caa57db00
Nghia Vu <nghiavu@192.168.0.100>
2024-08-28 00:28:15 +0700

    implement firebase

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,46 +1,54 @@
-import { useState, useEffect } from 'react';
-import JobListing from './JobListing';
-import Spinner from './Spinner';
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import JobListing from "./JobListing";
+import Spinner from "./Spinner";
+import { db } from "../firebase";
 
 const JobListings = ({ isHome = false }) => {
-  const [jobs, setJobs] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchJobs = async () => {
-      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
-      try {
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-        setJobs(data);
-      } catch (error) {
-        console.log('Error fetching data', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+	useEffect(() => {
+		const fetchJobs = async () => {
+			const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+			try {
+				// const res = await fetch(apiUrl);
+				// const data = await res.json();
+				// setJobs(data);
+				const firebaseData = await getDocs(collection(db, "jobs"));
+				const firebaseJobs = firebaseData.docs.map((doc) => ({
+					id: doc.id,
+					...doc.data(),
+				}));
+				setJobs(firebaseJobs);
+			} catch (error) {
+				console.log("Error fetching data", error);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    fetchJobs();
-  }, []);
+		fetchJobs();
+	}, []);
 
-  return (
-    <section className='bg-blue-50 px-4 py-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
-        </h2>
+	return (
+		<section className='bg-blue-50 px-4 py-10'>
+			<div className='container-xl lg:container m-auto'>
+				<h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+					{isHome ? "Recent Jobs" : "Browse Jobs"}
+				</h2>
 
-        {loading ? (
-          <Spinner loading={loading} />
-        ) : (
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobs.map((job) => (
-              <JobListing key={job.id} job={job} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
-  );
+				{loading ? (
+					<Spinner loading={loading} />
+				) : (
+					<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+						{jobs.map((job) => (
+							<JobListing key={job.id} job={job} />
+						))}
+					</div>
+				)}
+			</div>
+		</section>
+	);
 };
 export default JobListings;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: "react-jobs-5e1d2.firebaseapp.com",
+	projectId: "react-jobs-5e1d2",
+	storageBucket: "react-jobs-5e1d2.appspot.com",
+	messagingSenderId: "665361308055",
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
